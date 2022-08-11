52041d015cdc1efcc095070f262f0db1359ee2b8
Tochukwu Ozurumba <ozurumbatochukwu@yahoo.com>
2022-08-11 04:27:17 -0400

    Dice History added

---
+++ app/src/main/java/com/tochukwuozurumba/dice/Die.java
@@ -0,0 +1,35 @@
+package com.tochukwuozurumba.dice;
+
+import android.util.Log;
+
+public class Die {
+    private int diceSides;
+    private int sideUp;
+    private String diceName;
+
+
+    public Die(int diceSides) {
+        this.diceSides = diceSides;
+        rollDice();
+    }
+
+    public int getSideUp () {
+        return sideUp;
+    }
+
+    public String getDiceName() {
+        return this.diceName;
+    }
+
+    public void setDiceName(String diceName) {
+        this.diceName = diceName;
+    }
+
+    public void rollDice() {
+        sideUp = (int) (Math.random() * diceSides) + 1;
+        setDiceName("d" + sideUp);
+        Log.d("die sideup", String.valueOf(sideUp));
+        Log.d("die number", String.valueOf(diceSides));
+        Log.d("TAG", diceName);
+    }
+}
--- app/src/main/java/com/tochukwuozurumba/dice/MainActivity.java
+++ app/src/main/java/com/tochukwuozurumba/dice/MainActivity.java
@@ -1,24 +1,30 @@
 package com.tochukwuozurumba.dice;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     //    declare global variables
     private Spinner spinner;
-//    private List<String> spinnerList;
     private String another;
     private Button customDieButton;
     private Button rollButtonOnce;
@@ -29,11 +35,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ArrayAdapter<Integer> spinnerAdapter;
     private Integer getCustomDieValue;
     private ArrayList<Integer> spinnerList;
+    private SharedPreferences prefs;
+    private int selectedSpinnerOption;
+    private Die dice;
+    private ArrayAdapter<String> historyAdapter;
+    private ArrayList<String> diceHistoryListArray;
+    private ListView diceHistoryDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        shared preferences
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
 
 //        define variables
         spinner = findViewById(R.id.spinner);
@@ -41,6 +56,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         customDieButton = findViewById(R.id.custom_die_button);
         rollButtonOnce = findViewById(R.id.roll_once_button);
         rollButtonTwice = findViewById(R.id.roll_twice_button);
+        rollOnceDisplay = findViewById(R.id.roll_once_score);
+        rollTwiceDisplay = findViewById(R.id.roll_twice_score);
 
 //        set button view instance
         customDieButton.setOnClickListener(this);
@@ -53,10 +70,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         spinnerList.add(4);
         spinnerList.add(6);
         spinnerList.add(8);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedSpinnerOption = spinnerList.get(position);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        //        instanstiate Die class
+        Log.d("select", String.valueOf(selectedSpinnerOption));
+        dice = new Die(selectedSpinnerOption);
+
+//        history
+        diceHistoryListArray = new ArrayList<>();
+        historyAdapter = new ArrayAdapter<String>(this, R.layout.recycler_view, diceHistoryListArray);
+        diceHistoryDisplay = findViewById(R.id.view_history);
+        diceHistoryDisplay.setAdapter(historyAdapter);
+    }
+
+    protected void onResume(){
+        super.onResume();
+
+//        Log.d("interger", String.valueOf(prefs.getInt("iniii", Integer.parseInt(""))));
+//        String stringSpinnerList = prefs.getString("prefs_spinner_list", "");
+//        Log.d("onResume", stringSpinnerList);
+
+
         spinnerAdapter = new ArrayAdapter<>(this, android.support.v7.appcompat.R.layout.support_simple_spinner_dropdown_item, spinnerList);
         spinner.setAdapter(spinnerAdapter);
-
     }
+
+//    protected void onPause() {
+//        saveData();
+//        Log.d("Pause", "App is paused");
+//        super.onPause();
+//    }
+//
+//    protected void onStop() {
+//        saveData();
+//        super.onStop();
+//    }
+//    protected void onDestroy() {
+//        saveData();
+//        super.onDestroy();
+//    }
 
     /**
      * Called when a view has been clicked.
@@ -70,6 +131,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (viewId) {
             case R.id.custom_die_button:
                 addCustomDie();
+                break;
+            case R.id.roll_once_button:
+                rollOnce();
+                break;
+            case R.id.roll_twice_button:
+                rollTwice();
         }
     }
 
@@ -80,12 +147,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //        add to custom die to list
         spinnerList.add(getCustomDieValue);
 
-        Log.d("SpinnerList: ", spinnerList.toString());
-
 //        notify adapter that the list has been updated
         spinnerAdapter.notifyDataSetChanged();
 
 //        reset custom input
         customDieInput.setText("");
     }
+
+    private void rollOnce() {
+        Log.d("select", String.valueOf(selectedSpinnerOption));
+        rollOnceDisplay.setText(String.valueOf(dice.getSideUp()));
+        rollTwiceDisplay.setVisibility(View.GONE);
+
+//        update history
+        diceHistoryListArray.add(dice.getDiceName());
+        historyAdapter.notifyDataSetChanged();
+    }
+
+    private void rollTwice() {
+        Log.d("select", String.valueOf(selectedSpinnerOption));
+//        set visibility true
+        if (rollTwiceDisplay.getVisibility() == View.GONE) { rollTwiceDisplay.setVisibility(View.VISIBLE); }
+
+//        display first value
+        rollOnceDisplay.setText(String.valueOf(dice.getSideUp()));
+        diceHistoryListArray.add(0, dice.getDiceName());
+
+//        roll for second value
+        dice.rollDice();
+
+//        display second value
+        rollTwiceDisplay.setText(String.valueOf(dice.getSideUp()));
+        diceHistoryListArray.add(dice.getDiceName());
+        historyAdapter.notifyDataSetChanged();
+    }
+
+
+
+//    private void saveData(){
+//        SharedPreferences.Editor editor = prefs.edit();
+//        editor.putInt("iniii", 4);
+//        Log.d("saveData", String.valueOf(spinnerList));
+//        editor.putString("prefs_spinner_list", String.valueOf(spinnerList));
+//        editor.apply();
+//    }
 }
